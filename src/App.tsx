1a0ea458363518a30702b4a4bd59177b2520352e
1lymax <59887205+1lymax@users.noreply.github.com>
2022-11-11 19:52:59 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MyWorks from "./Pages/MyWorks";
+import {myWorks} from "./utils/data";
+
+import './App.scss'
+import Footer from "./Pages/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='main'>
+            <MyWorks works={myWorks}/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
