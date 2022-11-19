420182e234189cf5802a4795e85548ccaa5b9ce7
1lymax <59887205+1lymax@users.noreply.github.com>
2022-11-19 09:41:51 +0200

    feat: about section

---
+++ src/Pages/Portrait.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Portrait.scss'
+
+const Portrait = () => {
+    return (
+        // <div className="portrait-wrapper">
+            <div className="portrait-card">
+            </div>
+        // </div>
+    );
+};
+
+export default Portrait;
