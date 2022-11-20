2439b153624d7991ce54ddfcfdf988bc390ce7db
1lymax <59887205+1lymax@users.noreply.github.com>
2022-11-20 14:33:58 +0200

    feat: backside of tile

--- src/Pages/MyWorks.tsx
+++ src/Pages/MyWorks.tsx
@@ -1,46 +1,63 @@
-import React, {FC, useEffect} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {VanillaTilt} from "../utils/vanilla-tilt";
 
 import './MyWorks.scss'
 
 interface MyWorksProps {
-	works: IWork[]
+    works: Array<{
+        id: string;
+        title: string;
+        image?: string | undefined;
+        description: string | React.ReactNode;
+        backface: {
+            link?: string;
+            description: string | React.ReactNode;
+        }
+    }>
 }
 
-interface IWork {
-	id: string;
-	title: string;
-	image?: string | undefined;
-	description: string | React.ReactNode
-}
+const MyWorks: FC<MyWorksProps> = ({works}) => {
+    const [back, setBack] = useState(-1);
 
-const MyWorks: FC <MyWorksProps> = ({works}) => {
-
-	useEffect(() => VanillaTilt.init(document.querySelectorAll(".card"), {
-		max: 25,
-		speed: 1000
-	}), [])
+    useEffect(() => VanillaTilt.init(document.querySelectorAll(".card"), {
+        max: 25,
+        speed: 1000
+    }), [])
 
 
-	return (
-		<div className="container">
-			{works.map(work =>
-				<div className="card" key={work.id}>
-					<div className="content">
-						<h2>{work.id}</h2>
-						<h3>{work.title}</h3>
-						<p>
-							{work.description}
-						</p>
-						{work?.image &&
-							<div className="image" style={{background: `url(${work.image})`, backgroundSize: 'cover'}}></div>
-						}
-						<a href="/">More info...</a>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+    return (
+        <div className="container">
+            {works.map((work, index) =>
+                <div className="card" key={work.id}>
+                    <div className={["front", back !== index ? "active" : "hidden"].join(" ")}
+                         onClick={() => setBack(index)}
+                    >
+                        <div className="front-content">
+                            <h2>{work.id}</h2>
+                            <h3>{work.title}</h3>
+                            <p>
+                                {work.description}
+                            </p>
+                            {work?.image &&
+								<div className="image"
+									 style={{background: `url(${work.image})`, backgroundSize: 'cover'}}></div>
+                            }
+                            <div className="more-info">
+                                More info...
+                            </div>
+                        </div>
+                    </div>
+                    <div className={["back", back === index ? "active" : "hidden"].join(" ")}
+                         onClick={() => setBack(-1)}>
+                        {work.backface.link &&
+                            <a href={work.backface.link} target="_blank" rel="noreferrer">{work.backface.link}</a>
+                        }
+                        {work.backface.description}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default MyWorks;
