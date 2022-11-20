5a4c88d94d87348aa2bb28ab5f43ed6821391b11
1lymax <59887205+1lymax@users.noreply.github.com>
2022-11-20 17:51:09 +0200

    refactor

---
+++ src/Pages/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './Footer.scss'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <ul className="sci">
+                <li><a href="/src/Pages"><i className="fa-brands fa-youtube"></i></a></li>
+                <li><a href="/src/Pages"><i className="fa-brands fa-instagram"></i></a></li>
+                <li><a href="/src/Pages"><i className="fa-brands fa-linkedin"></i></a></li>
+                <li><a href="/src/Pages"><i className="fa-brands fa-github"></i></a></li>
+                <li><a href="/src/Pages"><i className="fa-brands fa-quora"></i></a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Footer;
