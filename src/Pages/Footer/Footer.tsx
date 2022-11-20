e0e7344390ce642364d6397890dd756815b9079f
1lymax <59887205+1lymax@users.noreply.github.com>
2022-11-20 18:35:31 +0200

    fixes

--- src/Pages/Footer/Footer.tsx
+++ src/Pages/Footer/Footer.tsx
@@ -6,11 +6,27 @@ const Footer = () => {
     return (
         <div className="footer">
             <ul className="sci">
-                <li><a href="/src/Pages"><i className="fa-brands fa-youtube"></i></a></li>
-                <li><a href="/src/Pages"><i className="fa-brands fa-instagram"></i></a></li>
-                <li><a href="/src/Pages"><i className="fa-brands fa-linkedin"></i></a></li>
-                <li><a href="/src/Pages"><i className="fa-brands fa-github"></i></a></li>
-                <li><a href="/src/Pages"><i className="fa-brands fa-quora"></i></a></li>
+                {/*<li><a href="/src/Pages"><i className="fa-brands fa-youtube"></i></a></li>*/}
+                <li>
+                    <a target="_blank" rel="noreferrer" href="/src/Pages">
+                    <i className="fa-brands fa-telegram"/>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/mlysogorov/">
+                        <i className="fa-brands fa-linkedin"/>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/1lymax">
+                    <i className="fa-brands fa-github"/>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="mailto:m.lysogorov@gmail.com">
+                        <i className="fa-solid fa-envelope"></i>
+                    </a>
+                </li>
             </ul>
         </div>
     );
